89a45e340dbd5ca01bc884f140c2a5a89a0e1609
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-06-24 19:11:04 +1000

    rendering person relationships in react

--- react-web/src/components/movie.js
+++ react-web/src/components/movie.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PeoplesRolesList from './PeopleRolesList'
 
 export default function Movie({
     title,
@@ -12,6 +13,12 @@ export default function Movie({
     <article>
         <h2>{ title } <small>({ yearReleased })</small></h2>
         <p>{ description }</p>
+        <h3>Cast</h3>
+        <PeoplesRolesList items={ cast } />
+        <h3>Writers</h3>
+        <PeoplesRolesList items={ writers } />
+        <h3>Directors</h3>
+        <PeoplesRolesList items={ directors } />
     </article>
     )
 }
